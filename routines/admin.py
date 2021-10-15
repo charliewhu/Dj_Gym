9e1c4b534fadba968df4fe4b3277bf9e49cea858
charl <charl@DESKTOP-A4DK2O9>
2021-10-15 13:09:45 +0100

    changed admin

--- routines/admin.py
+++ routines/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from django.apps import apps
+from . import models
 
-app = apps.get_app_config('routines')
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+
+class ExerciseAdmin(admin.TabularInline):
+    model = models.Exercise
+
+class MuscleGroupAdmin(admin.ModelAdmin):
+   inlines = [ExerciseAdmin,]
+
+
+
+admin.site.register(models.MuscleGroup, MuscleGroupAdmin)
+admin.site.register(models.Exercise)
+admin.site.register(models.CycleType)
+admin.site.register(models.Workout)
+admin.site.register(models.WorkoutItem)
+
+
+
+
+
