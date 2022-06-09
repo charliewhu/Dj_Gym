ff10d74dd6bcb594e396919115f1aba14bf59084
Charlie <charlieavery123@hotmail.co.uk>
2022-06-09 14:24:29 +0100

    initialised first routines test

---
+++ routines/tests/models/workoutexerciseset/test_set_generate.py
@@ -0,0 +1,8 @@
+"""
+GIVEN a workout_exercise_set
+and workout_exercise.is_set_generate = True
+WHEN the workout_exercise_set is created
+AND the workout_exercise_set is not within rep range
+AND the workout_exercise_set is not within RIR range
+THEN the following set should be created
+"""
